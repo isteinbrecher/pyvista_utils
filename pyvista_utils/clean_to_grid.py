42201b74ced5cf00317ef2f4e277477aca8fb65a
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2023-03-12 15:14:55 +0100

    Working clean to grid

--- pyvista_utils/clean_to_grid.py
+++ pyvista_utils/clean_to_grid.py
@@ -12,10 +12,13 @@ def clean_to_grid(mesh_in, **kwargs):
     unique_indices, inverse_indices = point_partners_to_unique_indices(
         partners, n_partners
     )
-    unique_id_count = np.zeros(len(unique_indices), dtype=int)
+    unique_points = points[unique_indices]
+    n_unique_points = len(unique_indices)
+
+    unique_id_count = np.zeros(n_unique_points, dtype=int)
     for unique_id in inverse_indices:
         unique_id_count[unique_id] += 1
-    unique_points = points[unique_indices]
+    unique_id_count_inverse = 1.0 / unique_id_count
 
     # Map the cell indices to the unique points
     cells_out = mesh_in.cells.copy()
@@ -27,6 +30,18 @@ def clean_to_grid(mesh_in, **kwargs):
         cells_out[my_slice] = [inverse_indices[id] for id in cells_out[my_slice]]
         index += number_of_points_connected_to_cell + 1
 
+    # Create the output mesh
     mesh_out = pyvista.UnstructuredGrid(cells_out, celltypes_out, unique_points)
+
+    # Set the data values
+    mesh_out.cell_data.update(mesh_in.cell_data)
+    for point_data_name, point_data_in in mesh_in.point_data.items():
+        point_data_out = np.zeros(point_data_in.shape)[:n_unique_points]
+        for i_original, i_unique in enumerate(inverse_indices):
+            # Todo move the scaling out of the loop and use a np function to do it
+            point_data_out[i_unique] += (
+                point_data_in[i_original] * unique_id_count_inverse[i_unique]
+            )
+        mesh_out.point_data[point_data_name] = point_data_out
 
     return mesh_out
