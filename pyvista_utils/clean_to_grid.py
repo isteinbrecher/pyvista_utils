8941f7d87c639434f37fe90957da2496f78d353c
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2023-09-24 17:22:32 +0200

    Remove dependency on meshpy

--- pyvista_utils/clean_to_grid.py
+++ pyvista_utils/clean_to_grid.py
@@ -1,17 +1,16 @@
 # -*- coding: utf-8 -*-
-"""
-Implement a clean_to_grid functionality similar to the one provided in
+"""Implement a clean_to_grid functionality similar to the one provided in
 ParaView. This allows to clean UnstructuredGrid (the clean command in 
 pyvista only works for PolyData).
 """
 
 import numpy as np
 import pyvista
-from meshpy.geometric_search import find_close_points, point_partners_to_unique_indices
+from scipy.spatial import KDTree
 
 
 def clean_to_grid(
-    mesh_in: pyvista.UnstructuredGrid, **kwargs
+    mesh_in: pyvista.UnstructuredGrid, *, tol: float = 1e-8
 ) -> pyvista.UnstructuredGrid:
     """Clean the mesh, i.e., overlapping points are replaced by a single point.
     Point data at overlapping points is averaged. Cell data is kept as it is,
@@ -25,50 +24,69 @@ def clean_to_grid(
     ----
     mesh_in:
         Mesh to be cleaned
-    **kwargs:
-        Optional arguments are passed to meshpy.geometric_search.find_close_points
+    tol: float
+        Tolerance to be used in the closest point search
     Return
     ----
     mesh_out:
         The cleaned mesh (a copy of the original mesh where double points are merged)
     """
 
-    # Find unique points
+    # Find points that are close to each other
     points = mesh_in.points
-    partners, n_partners = find_close_points(points, **kwargs)
-    unique_indices, inverse_indices = point_partners_to_unique_indices(
-        partners, n_partners
+    kd_tree = KDTree(points)
+    pairs = kd_tree.query_pairs(r=tol, output_type="ndarray")
+
+    # Group together all point ids of points that are at the same spatial dimension
+    point_id_connectivity = [{i} for i in range(len(points))]
+    for pair in pairs:
+        point_id_connectivity[pair[0]].add(pair[1])
+        point_id_connectivity[pair[1]].add(pair[0])
+    point_id_connectivity = frozenset(
+        [frozenset(item) for item in point_id_connectivity]
     )
-    unique_points = points[unique_indices]
-    n_unique_points = len(unique_indices)
 
-    unique_id_count = np.zeros(n_unique_points, dtype=int)
-    for unique_id in inverse_indices:
-        unique_id_count[unique_id] += 1
-    unique_id_count_inverse = 1.0 / unique_id_count
+    # Create a mapping from the original ids to the new (spatially) unique ids.
+    # Also get the new point coordinates and count how many old points a new point
+    # "unifies".
+    n_cleaned_points = len(point_id_connectivity)
+    point_id_mapping_old_to_new = [None for i in range(len(points))]
+    cleaned_points = np.zeros((n_cleaned_points, 3), dtype=points.dtype)
+    cleaned_points_count = np.zeros(n_cleaned_points, dtype=int)
+    for unique_index, item in enumerate(point_id_connectivity):
+        for index in item:
+            if point_id_mapping_old_to_new[index] is None:
+                point_id_mapping_old_to_new[index] = unique_index
+                cleaned_points[unique_index] = points[index]
+                cleaned_points_count[unique_index] += 1
+            else:
+                raise ValueError(
+                    "There are connected clusters of points. This can happen if the tolerance is to high."
+                )
+    cleaned_points_count_inverse = 1.0 / cleaned_points_count
 
-    # Map the cell indices to the unique points
+    # Get the new cell connectivity
     cells_out = mesh_in.cells.copy()
     celltypes_out = mesh_in.celltypes.copy()
     index = 0
     while index < len(cells_out):
         number_of_points_connected_to_cell = cells_out[index]
         my_slice = slice(index + 1, index + number_of_points_connected_to_cell + 1)
-        cells_out[my_slice] = [inverse_indices[id] for id in cells_out[my_slice]]
+        cells_out[my_slice] = [
+            point_id_mapping_old_to_new[id] for id in cells_out[my_slice]
+        ]
         index += number_of_points_connected_to_cell + 1
 
     # Create the output mesh
-    mesh_out = pyvista.UnstructuredGrid(cells_out, celltypes_out, unique_points)
+    mesh_out = pyvista.UnstructuredGrid(cells_out, celltypes_out, cleaned_points)
 
     # Set the data values
     mesh_out.cell_data.update(mesh_in.cell_data)
     for point_data_name, point_data_in in mesh_in.point_data.items():
-        point_data_out = np.zeros(point_data_in.shape)[:n_unique_points]
-        for i_original, i_unique in enumerate(inverse_indices):
-            # Todo move the scaling out of the loop and use a np function to do it
-            point_data_out[i_unique] += (
-                point_data_in[i_original] * unique_id_count_inverse[i_unique]
-            )
+        point_data_out = np.zeros((n_cleaned_points, 3))
+        for i_original, i_unique in enumerate(point_id_mapping_old_to_new):
+            point_data_out[i_unique] += point_data_in[i_original]
+        point_data_out *= cleaned_points_count_inverse[:, np.newaxis]
         mesh_out.point_data[point_data_name] = point_data_out
 
     return mesh_out
