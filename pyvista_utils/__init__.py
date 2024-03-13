42e584bd5c1dbcf6c8f7e5ff674347a0dc6c38c9
Ivo Steinbrecher <ivo.steinbrecher@unibw.de>
2024-03-13 17:09:00 +0100

    Remove clean to grid

--- pyvista_utils/__init__.py
+++ pyvista_utils/__init__.py
@@ -3,7 +3,6 @@
 Define the main namespace of pyvista_utils
 """
 
-from .clean_to_grid import clean_to_grid
 from .compare_grids import compare_grids
 from .merge_polylines import merge_polylines
 from .polyline_cross_section import polyline_cross_section
